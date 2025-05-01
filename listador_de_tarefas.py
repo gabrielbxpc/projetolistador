1af11c196c9c1308ac5255f282ddc9abb7191512
gabrielbxpc <gabrielbxpc@gmail.com>
2025-05-01 18:45:58 -0300

    terminando o salvamento

--- listador_de_tarefas.py
+++ listador_de_tarefas.py
@@ -1,48 +1,59 @@
-class Tarefa:
-    def __init__(self, pendente = True):
-        self.pendente = pendente
-
-
-
+import datetime
+import os
+import json
 
 # git add -> adicionar aquivo para commit
 # git commit -m "<message>" -> commitar de fato nova versão com modificações
 
+data = datetime.datetime.now()
 ze = 0
 
-# alga = {"Título": [], "Tarefas": []}
 listatitulos = []
 listadesc = []
-listadata = []
+
+with open("meus_dados.json" "w") as arquivo:
+    json.dump(listatitulos)
+    json.dump(listadesc)
+
+with open("meus_dados.json" "r") as arquivo2:
+    listatitulos.append(json.load(arquivo2))
+    listadesc.append(json.load(arquivo2))
 
 while ze == 0:
+
+    os.system('cls')
 
     print('-=-' * 20)
     print('LISTADOR DE TAREFAS')
     print('-=-' * 20)
-    print(''
-
-          '')
+    print("""
+    """)
     print("(1) listar nova tarefa "
           "(2) verificar tarefas"
           "(3) fechar o programa"
           )
 
-    user_input = int(input("Selecione uma opção acima: "))
+    user_input = (input("Selecione uma opção acima: ")).strip()
 
-    if user_input == 1:
+    if user_input == '1':
         titulo = input('Título: ').title().strip()
         tarefa = input('digite sua tarefa: ').strip()
-        data = input('data que você está listando a tarefa: ').strip()
         td = input('digite enter para voltar ao programa: ')
         listatitulos.append(titulo)
         listadesc.append(tarefa)
-        listadata.append(data)
+        with open("meus_dados.json", "w") as arquivo:
+            object_json = json.dumps(listatitulos, indent=4)
+            object_json2 = json.dumps(listadesc, indent=4)
 
-    if user_input == 2:
+
+
+    elif user_input == '2':
         for index, t in enumerate(listatitulos, start=0):
-            print(index, t, "(data da tarefa listada): ", listadata, '[TAREFA PENDENTE]')
+            print('[', index, ']', t, "| data da tarefa listada: ", data.strftime("%d-%m-%Y às %H : %M : %S"), '[TAREFA PENDENTE]')
 
-    if user_input== 3:
+    elif user_input== '3':
         ze = 1
 
+    else:
+        print('[ OPÇÃO NÃO LISTADA VOLTANDO AO PROGRAMA ]')
+        print('...'*39)
