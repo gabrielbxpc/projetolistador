b4f437a1f72862a3c282d79e3164e76296a11a87
gabrielbxpc <gabrielbxpc@gmail.com>
2025-05-01 15:37:06 -0300

    atualizei as funções do programa

--- listador_de_tarefas.py
+++ listador_de_tarefas.py
@@ -1,8 +1,19 @@
+class Tarefa:
+    def __init__(self, pendente = True):
+        self.pendente = pendente
+
+
+
+
+# git add -> adicionar aquivo para commit
+# git commit -m "<message>" -> commitar de fato nova versão com modificações
+
 ze = 0
 
 # alga = {"Título": [], "Tarefas": []}
 listatitulos = []
 listadesc = []
+listadata = []
 
 while ze == 0:
 
@@ -14,18 +25,24 @@ while ze == 0:
           '')
     print("(1) listar nova tarefa "
           "(2) verificar tarefas"
-          "(3) excluir tarefas"
-          "(4) fechar o programa")
+          "(3) fechar o programa"
+          )
 
     user_input = int(input("Selecione uma opção acima: "))
 
     if user_input == 1:
-        titulo = input('Título: ').title()
-        tarefa = input('digite sua tarefa: ')
+        titulo = input('Título: ').title().strip()
+        tarefa = input('digite sua tarefa: ').strip()
+        data = input('data que você está listando a tarefa: ').strip()
         td = input('digite enter para voltar ao programa: ')
         listatitulos.append(titulo)
         listadesc.append(tarefa)
+        listadata.append(data)
 
     if user_input == 2:
         for index, t in enumerate(listatitulos, start=0):
-            print(index, t)
+            print(index, t, "(data da tarefa listada): ", listadata, '[TAREFA PENDENTE]')
+
+    if user_input== 3:
+        ze = 1
+
